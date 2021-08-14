4147723a5922ba12e2533ede23f2d68ffd48e46d
Jacques Van Blokland <jvanblokland@catalyte.io>
2021-08-13 22:36:55 -0700

    Added Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { cx, css } from '@emotion/css'
+
+const Home = () => {
+    return (
+        <>
+            <h1>The Homepage of Jacques Van Blokland</h1>
+        </>
+    )
+}
+
+export default Home
