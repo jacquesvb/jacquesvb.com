c13725ca6c77a69aa8d7d96fc1a5d7535281a444
Jacques Van Blokland <jvanblokland@catalyte.io>
2021-08-13 21:39:17 -0700

    Removed CMS from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ module.exports = {
     title: "jacquesvb.com",
   },
   plugins: [
-    "gatsby-plugin-netlify-cms",
     "gatsby-plugin-emotion",
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
